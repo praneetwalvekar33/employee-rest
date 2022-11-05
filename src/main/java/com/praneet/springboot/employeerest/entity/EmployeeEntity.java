6af834fb7d45b7fd0972b32efa5fda4aa8d73072
praneetwalvekar33 <praneetwalvekar@gmail.com>
2022-11-05 22:09:44 +0530

    Initial commit

---
+++ src/main/java/com/praneet/springboot/employeerest/entity/EmployeeEntity.java
@@ -0,0 +1,78 @@
+package com.praneet.springboot.employeerest.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employee")
+public class EmployeeEntity {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="email")
+	private String email;
+	
+	public EmployeeEntity(){
+		
+	}
+	
+	public EmployeeEntity(String theFirstName, String theLastName, String theEmail) {
+		this.firstName=theFirstName;
+		this.lastName=theLastName;
+		this.email=theEmail;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "EmployeeEntity [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ "]";
+	}
+	
+	
+	
+
+}
---
+++ src/test/java/com/praneet/springboot/employeerest/EmployeerestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praneet.springboot.employeerest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeerestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
